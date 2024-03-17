335d3380628b0e0915369ba9dce51f785e0cc961
Indrasenareddy4966 <163498141+Indrasenareddy4966@users.noreply.github.com>
2024-03-17 19:32:14 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,17 @@
+import random
+import string
 
+def generate_password(length):
+    characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+    random.shuffle(characters)
+    password = ''.join(random.choice(characters, k=length))
+    return password
+
+def main():
+   
+    length = int(input("Enter the desired  password length : ")) 
+    password = generate_password(length)
+    print("Your password is: {password}") 
+        
+if __name__ == "__main__":
+    main()
